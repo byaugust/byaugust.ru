dbd7946962a0e1e4e4e58185c4cb71e67e750ea4
Константин Михайлович Вороной <konstantinvoronoy@gmail.com>
2021-08-11 11:24:40 +0300

    update documenting

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -63,7 +63,7 @@ async function createApp(ssrContext, config = {}) {
   // here we inject the router and store to all child components,
   // making them available everywhere as `this.$router` and `this.$store`.
   const app = {
-    head: {"title":"Константин Вороной","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"My super-excellent Nuxt.js project"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
+    head: {"title":"Константин Вороной","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"My super-excellent Nuxt.js project"}],"link":[],"style":[],"script":[]},
 
     router,
     nuxt: {
