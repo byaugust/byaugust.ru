b5afc3d2ece8a2fcb270172894f031c1c016febb
Konstantin Voronoy <konstantinvoronoy@gmail.com>
2022-07-17 20:09:13 +0600

    fix: padding of default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,6 @@
 <template>
 	<div class="w-full h-full bg-white dark:bg-gray-900">
 		<baseNavbar />
-		<div class="w-full mx-auto px-6 py-6">
-			<slot />
-		</div>
+		<slot />
 	</div>
 </template>
